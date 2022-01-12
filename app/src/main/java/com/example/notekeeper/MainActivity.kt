cdeab9597d60abab072d36abbc256c5360da36ce
SammyBloom <nwachiemi@gmail.com>
2022-01-12 14:53:43 +0100

    Using Intent to pass data

--- app/src/main/java/com/example/notekeeper/MainActivity.kt
+++ app/src/main/java/com/example/notekeeper/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
+import android.widget.Spinner
 import android.widget.TextView
 import com.example.notekeeper.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
@@ -12,20 +13,46 @@ import com.google.android.material.snackbar.Snackbar
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private var notePosition = POSITION_NOT_SET
+
+    private var textNoteTitle : TextView? = null
+    var textNoteText : TextView? = null
+    var spinnerCourses: Spinner? = null
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        setSupportActionBar(binding.toolbar)
 
         val mainBinding = binding.mainContainer
 
-        val dm = DataManager()
+        textNoteTitle = findViewById(R.id.textNoteTitle)
+        textNoteText = findViewById(R.id.textNoteText)
+        val spinnerCourses : Spinner = findViewById(R.id.spinnerCourses)
+
         val adapterCourses = ArrayAdapter<CourseInfo>(this,
                 android.R.layout.simple_spinner_item,
-                dm.courses.values.toList())
+                DataManager.courses.values.toList())
         adapterCourses.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
-        mainBinding.spinnerCourses.adapter = adapterCourses
+        spinnerCourses.adapter = adapterCourses
+
+        notePosition = intent.getIntExtra(EXTRA_NOTE_POSITION, POSITION_NOT_SET)
+
+        if (notePosition != POSITION_NOT_SET){
+            displayNote()
+        }
+    }
+
+    private fun displayNote() {
+        val note = DataManager.notes[notePosition]
+        textNoteTitle?.text = note.title
+        textNoteText?.text = note.text
+
+        val coursePosition = DataManager.courses.values.indexOf(note.course)
+        spinnerCourses?.setSelection(coursePosition)
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -34,6 +61,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        return super.onOptionsItemSelected(item)
+        return when (item.itemId){
+            R.id.action_settings -> true
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
---
+++ app/src/main/java/com/example/notekeeper/NoteListActivity.kt
@@ -0,0 +1,42 @@
+package com.example.notekeeper
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.notekeeper.databinding.ActivityNoteListBinding
+
+class NoteListActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityNoteListBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityNoteListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.fab.setOnClickListener { view ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            startActivity(activityIntent)
+        }
+
+        val listNotes = findViewById<ListView>(R.id.listNotes)
+        listNotes.adapter = ArrayAdapter(this,
+            android.R.layout.simple_list_item_1, DataManager.notes)
+
+        listNotes.setOnItemClickListener{parent, view, position, id ->
+            val activityIntent = Intent(this, MainActivity::class.java)
+            activityIntent.putExtra(EXTRA_NOTE_POSITION, position)
+            startActivity(activityIntent)
+        }
+    }
+
+}
