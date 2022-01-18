f6cbec6b2c08401c9cbcd7ca769d779c3714b2db
SammyBloom <nwachiemi@gmail.com>
2022-01-18 17:14:15 +0100

    Using Intent to pass data

--- app/src/main/java/com/example/notekeeper/MainActivity.kt
+++ app/src/main/java/com/example/notekeeper/MainActivity.kt
@@ -1,5 +1,6 @@
  package com.example.notekeeper
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
@@ -7,6 +8,7 @@ import android.view.MenuItem
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import com.example.notekeeper.databinding.ActivityMainBinding
 import com.google.android.material.snackbar.Snackbar
 
@@ -63,7 +65,41 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when (item.itemId){
             R.id.action_settings -> true
+            R.id.action_next -> {
+                moveNext()
+                true
+            }
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    private fun moveNext() {
+        ++notePosition
+        displayNote()
+        invalidateOptionsMenu()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onPrepareOptionsMenu(menu: Menu?): Boolean {
+        if(notePosition >= DataManager.notes.lastIndex){
+            val menuItem = menu?.findItem(R.id.action_next)
+            if(menuItem != null){
+                menuItem.icon = getDrawable(R.drawable.ic_baseline_block_24)
+                menuItem.isEnabled = false
+            }
+        }
+        return super.onPrepareOptionsMenu(menu)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        saveNote()
+    }
+
+    private fun saveNote() {
+       val note = DataManager.notes[notePosition]
+       note.title = textNoteTitle?.text.toString()
+        note.text = textNoteText?.text.toString()
+       note.course = spinnerCourses?.selectedItem as CourseInfo
     }
 }
